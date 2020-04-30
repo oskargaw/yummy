2d8342785fe937c26ca37da348c4341c899b9277
szywyk <47740194+szywyk@users.noreply.github.com>
2020-05-01 01:48:56 +0200

    add desktop and mobile homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+		<div id="up" class="text-bold">The only thing...</div>
+    <img src="../assets/burger.jpg" id="burger">
+		<div class="has-search">
+			<input type="text" class="search" placeholder="Search" />
+		  <button class="lens"><i class="fa fa-search"></i></button>
+		</div>
+		<div id="like" class="text-bold">I like</div>
+    <div id="better">better than talking</div>
+		<div id="about">about food</div>
+    <div id="eating" class="text-bold">is eating</div>
+		<div id="everyone">~ literally everyone</div>
+	</div>	
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+<style lang="scss">
+@import "@/assets/scss/Home.scss";
+</style>
